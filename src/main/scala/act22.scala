a95008f44f56fc4de0d128732997b5dcc115e42d
Minha02 <fathumaminha@hmail.com>
2024-06-25 00:06:33 +0530

    Second Commit for the activity 2.2

--- src/main/scala/act22.scala
+++ src/main/scala/act22.scala
@@ -1,8 +1,18 @@
 object act22 {
-  var a = 2
-  var b = 3
-  var c = 4
-  var d = 5
-  var k = 4.3f
+  def main(args: Array[String]):Unit = {
+    var a = 2
+    var b = 3
+    var c = 4
+    var d = 5
+    var k = 4.3f
+    var g = 4.0f
 
-}
+    println((b-1) * a + c * (d-1))
+    val aval= a+1
+    println(aval)
+    println(-2*(g-k)+c)
+    c +=1
+    println(c)
+    println((c+1)*c*(a+1))
+
+  }}
