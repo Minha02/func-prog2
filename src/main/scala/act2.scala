d5cffe2f6a26d696e9549e20c10a5315340a0f4f
Minha02 <fathumaminha@hmail.com>
2024-06-24 11:25:00 +0530

    First Commit for the activity 2

---
+++ src/main/scala/act2.scala
@@ -0,0 +1,15 @@
+object act2 {
+  var i = 2
+  var j = 2
+  var m = 5
+  var n = 5
+  var k = 2
+  var f =12.0f
+  var g = 4.0f
+  var c = 'X'
+
+  def main(args: Array[String]):Unit = {
+    println(k +12*m)
+  }
+}
+
