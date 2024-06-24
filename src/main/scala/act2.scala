49be85b76c7455fe7a0e710cbbeca46a13e6395e
Minha02 <fathumaminha@hmail.com>
2024-06-24 21:17:52 +0530

    Second Commit for the activity 2.1

--- src/main/scala/act2.scala
+++ src/main/scala/act2.scala
@@ -1,4 +1,5 @@
 object act2 {
+  def main(args: Array[String]):Unit = {
   var i = 2
   var j = 2
   var m = 5
@@ -8,8 +9,19 @@ object act2 {
   var g = 4.0f
   var c = 'X'
 
-  def main(args: Array[String]):Unit = {
-    println(k +12*m)
-  }    
+  val ans_a = k +12*m
+  val ans_b = m/j
+  val ans_c = n%j
+  val ans_d = m/j*j
+  val ans_e = f + 10*5 + g
+  val ans_f = {i+= 1; i*n}
+
+    println(s"k +12*m = $ans_a")
+    println(s"m/j = $ans_b ")
+    println(s"n%j = $ans_c")
+    println(s"m/j*j = $ans_d")
+    println(s"f + 10*5 + g = $ans_e")
+    println(s"++i* n = $ans_f")
+  }      
 }
 
