ce1a8b0139ebd63b4ece755e600836c23ed68b21
Minha02 <fathumaminha@hmail.com>
2024-07-01 22:11:59 +0530

    First Commit - 3

--- project/Act3.scala
+++ project/Act3.scala
@@ -1,3 +1,10 @@
 object Act3 {
+  def reverseString(str: String): String = {
+    if (str.isEmpty) ""
+    else reverseString(str.tail) + str.head
+  }
 
+  def main(args: Array[String]): Unit = {
+    println("Reversed String: " + reverseString("HELLO"))
+  }
 }
